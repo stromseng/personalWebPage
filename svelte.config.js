f57255f3c9720427045bba7978a505cb42848a57
Thrywyn <magnus.stromseng@gmail.com>
2023-06-06 17:49:57 +0200

    fix build for dynamic slug pages

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,12 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		prerender: {
+			// use relative URLs similar to an anchor tag <a href="/test/1"></a>
+			// do not include group layout folders in the path such as /(group)/test/1
+			entries: ['/projects/lendables', '/projects/zdmap', '/projects/mywebsite', '*']
+		}
 	}
 };
 export default config;
