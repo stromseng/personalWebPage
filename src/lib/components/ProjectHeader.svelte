f6242b1690df5cfde3bbe2ad9ddbaf40fe5c1454
Thrywyn <magnus.stromseng@gmail.com>
2023-06-06 17:49:48 +0200

    add project deployed url

--- src/lib/components/ProjectHeader.svelte
+++ src/lib/components/ProjectHeader.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
-    export let title: string;
-    export let appStack: string;
-    export let githubUrl: string;
+	export let title: string;
+	export let appStack: string;
+	export let githubUrl: string;
+	export let deployedUrl: string;
 </script>
 
 <div class="">
+	<h1 class="h1">{title}</h1>
 
-        <h1 class="h1">{title}</h1>
-
-    <div class="mb-1"><code class="code">{appStack} </code> </div>
-    <a href={githubUrl}>
-        <div class="chip variant-soft-primary p-1">
-            <span><i class="fa-brands fa-github fa-xl"></i></span>
-            <span>GitHub</span>
-        </div>
-    </a>
-    
-
-
-    
+	<div class="mb-1"><code class="code">{appStack} </code></div>
+	{#if githubUrl}
+		<a href={githubUrl}>
+			<div class="chip variant-soft-primary p-1">
+				<span><i class="fa-brands fa-github fa-lg" /></span>
+				<span>GitHub</span>
+			</div>
+		</a>
+	{/if}
+	{#if deployedUrl}
+		<a href={deployedUrl}>
+			<div class="chip variant-soft-primary p-1">
+				<span><i class="fa-solid fa-globe fa-lg" /></span>
+				<span>Web Page</span>
+			</div>
+		</a>
+	{/if}
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-    import BodyDiv from "$lib/components/BodyDiv.svelte";
-    import { CodeBlock } from '@skeletonlabs/skeleton';
-    import { Avatar } from '@skeletonlabs/skeleton';
-    import ProjectCard from "$lib/components/ProjectCard.svelte";
-	import ProjectCardsGrid from "$lib/components/ProjectCardsGrid.svelte";
-    let ProfilePic = "/images/profile-pic.jpg"
+	import BodyDiv from '$lib/components/BodyDiv.svelte';
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import { Avatar } from '@skeletonlabs/skeleton';
+	import ProjectCard from '$lib/components/ProjectCard.svelte';
+	import ProjectCardsGrid from '$lib/components/ProjectCardsGrid.svelte';
+	let ProfilePic = '/images/profile-pic.jpg';
 </script>
 
 <BodyDiv>
-    <h1 class="h1">Projects</h1>
-    <ProjectCardsGrid></ProjectCardsGrid>
+	<h1 class="h1">Projects</h1>
+	<ProjectCardsGrid />
 </BodyDiv>
