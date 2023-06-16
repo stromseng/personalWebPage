3b3c29a4e174e4f33bdf63fb47181406dd103676
Magnus Alexander Strømseng <magnastr@stud.ntnu.no>
2023-06-16 12:47:43 +0200

    add navbar divider

--- src/lib/components/navbar/NavBarItem.svelte
+++ src/lib/components/navbar/NavBarItem.svelte
@@ -1,24 +1,26 @@
-<script lang='ts'>
-    import { Drawer,  drawerStore } from '@skeletonlabs/skeleton';
-    export let href: string = "";
+<script lang="ts">
+	import { Drawer, drawerStore } from '@skeletonlabs/skeleton';
+	export let href: string = '';
 
-
-    function drawerClose() {
-        drawerStore.close();
-    }
+	function drawerClose() {
+		drawerStore.close();
+	}
 </script>
-
-<style>
-    /* your styles go here */
-</style>
 
 <!-- markup (zero or more items) goes here -->
 
-<a href={href} on:click={drawerClose}>
-    <div class="w-full hover:bg-primary-hover-token h-24 flex flex-row place-content-center items-center">
-            <span class="mr-1">
-                <slot name="icon"><span></span></slot>
-            </span>
-            <slot><!-- optional fallback --></slot>
-    </div>
+<a {href} on:click={drawerClose}>
+	<div
+		class="w-full hover:bg-primary-hover-token h-16 flex flex-row place-content-center items-center"
+	>
+		<span class="mr-1">
+			<slot name="icon"><span /></slot>
+		</span>
+		<slot><!-- optional fallback --></slot>
+	</div>
 </a>
+<hr />
+
+<style>
+	/* your styles go here */
+</style>
