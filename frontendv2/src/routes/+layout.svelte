3c9710a718560f15b1a253521abdd0e4421437be
Magnus Strømseng <magnus.stromseng@gmail.com>
2023-11-27 22:30:09 +0100

    setup better navbar with drawer

--- frontendv2/src/routes/+layout.svelte
+++ frontendv2/src/routes/+layout.svelte
@@ -2,44 +2,54 @@
     import "../app.css";
 </script>
 
-<div class="navbar bg-base-300">
-    <div class="navbar-start">
-        <!--Hidden Dropdown Navbar-->
-        <div class="dropdown">
-            <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
-                </svg>
+
+<div class="drawer">
+    <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col">
+        <!-- Navbar -->
+        <div class="w-full navbar bg-base-300">
+            <div class="flex-none md:hidden">
+                <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         class="inline-block w-6 h-6 stroke-current">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </label>
             </div>
-            <ul class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a href="/about">About</a></li>
-                <li><a href="/projects">Projects</a></li>
-                <li>
-                    <a>Parent</a>
-                    <ul class="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                    </ul>
-                </li>
-                <li><a>Item 3</a></li>
-            </ul>
+            <div class="navbar-start">
+                <div class="btn btn-ghost hidden md:flex">
+                    <div class="avatar">
+                        <div class="w-10 rounded-full">
+                            <img src="/images/profile-pic.jpg" alt="Magnus Strømseng" />
+                        </div>
+                    </div>
+                    <a href="/" class="text-xl">Magnus A. Strømseng</a>
+                </div>
+            </div>
+            <div class="navbar-center hidden md:flex">
+                <ul class="menu menu-horizontal px-1">
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/projects">Projects</a></li>
+                </ul>
+            </div>
+            <div class="navbar-end">
+                <a class="btn btn-ghost btn-circle" href="https://github.com/Thrywyn">
+                    <i class="fa-brands fa-github fa-2xl"></i></a>
+                <a class="btn btn-ghost btn-circle"
+                   href="https://www.linkedin.com/in/magnus-alexander-str%C3%B8mseng-063112214/">
+                    <i class="fa-brands fa-linkedin fa-xl"></i></a>
+            </div>
         </div>
-        <div>
-            <a href="/" class="btn btn-ghost text-xl">Magnus A. Strømseng</a>
-        </div>
+        <slot />
     </div>
-    <!--Navbar Menu-->
-    <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal px-1">
+    <div class="drawer-side">
+        <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+        <ul class="menu p-4 w-80 min-h-full bg-base-200">
+            <!-- Sidebar content here -->
+            <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/projects">Projects</a></li>
         </ul>
     </div>
-    <div class="navbar-end">
-        <a class="btn btn-ghost btn-circle"><i class="fa-brands fa-github fa-2xl"></i></a>
-        <a class="btn btn-ghost btn-circle"><i class="fa-brands fa-linkedin fa-xl"></i></a>
-    </div>
 </div>
-
-<slot />
