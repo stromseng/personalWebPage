744bad1d9b1cb916c8a478cddf341d7920a686f6
Magnus Strømseng <magnus.stromseng@gmail.com>
2023-11-28 14:35:55 +0100

    feat: fiex navbar, darkmode selfDrawingLogo

--- frontendv2/src/routes/+layout.svelte
+++ frontendv2/src/routes/+layout.svelte
@@ -18,7 +18,7 @@
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content flex flex-col">
         <!-- Navbar -->
-        <div class="w-full navbar bg-base-300">
+        <div class="w-full navbar fixed bg-base-100 z-30">
             <div class="flex-none md:hidden">
                 <label
                     for="my-drawer"
@@ -104,7 +104,7 @@
         </div>
         <slot />
     </div>
-    <div class="drawer-side">
+    <div class="drawer-side z-40">
         <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"
         ></label>
         <ul class="menu p-4 w-80 min-h-full bg-base-200">
