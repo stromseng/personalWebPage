38fa45c6cf4cd1706fb8e085660a060f55fa89b3
Magnus Strømseng <magnus.stromseng@gmail.com>
2023-12-04 15:24:46 +0100

    feat: tailwind dark selector support, fix logocloud theme

--- frontend/src/lib/components/Styling/LogoSquare.svelte
+++ frontend/src/lib/components/Styling/LogoSquare.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div
-    class="bg-neutral rounded-xl flex flex-row items-center h-12 p-3 gap-2 justify-center">
+    class="bg-base-200 rounded-xl flex flex-row items-center h-12 p-3 gap-2 justify-center">
     <slot>
         <i class="fa-brands fa-css3-alt fa-2xl" style="color: #214ce5" />
         <h3 class="">CSS</h3>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -19,4 +19,5 @@ export default {
             },
         ],
     },
+    darkMode: ["class", '[data-theme="dark"]'],
 };
