df99e64e4db0b7d50527c90a2fd775767bbb58c0
Magnus Strømseng <magnus.stromseng@gmail.com>
2024-01-19 00:31:43 +0100

    improve vision

--- frontend/src/lib/components/ScrollableHeroes/ScrollableHeroes.svelte
+++ frontend/src/lib/components/ScrollableHeroes/ScrollableHeroes.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import Icon from '@iconify/svelte';
-    
+    import Icon from "@iconify/svelte";
+
     import { onMount } from "svelte";
     import { gsap } from "gsap";
     import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
@@ -8,27 +8,42 @@
     gsap.registerPlugin(ScrollTrigger);
 
     onMount(() => {
-        gsap.to(".hero", {
-            scrollTrigger: {
-                trigger: ".hero",
-                pin: true, // pin the ".hero-content" element while scrolling
-                start: "top top", // when the top of the trigger hits the top of the viewport
-                end: "+=100%", // end after scrolling the height of the element
-                scrub: true, // smooth scrolling effect
-            },
-        });
+        // gsap.to(".hero", {
+        //     scrollTrigger: {
+        //         trigger: ".hero",
+        //         pin: true, // pin the ".hero-content" element while scrolling
+        //         start: "top top", // when the top of the trigger hits the top of the viewport
+        //         end: "+=100%", // end after scrolling the height of the element
+        //         scrub: true, // smooth scrolling effect
+        //     },
+        // });
     });
 </script>
 
-<div class="hero min-h-96">
-    <div class="hero-content flex-col lg:flex-row-reverse">
-        <div>
-            <h1 class="text-5xl font-bold">I have experience in</h1>
-            <p class="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-            </p>
+<div class="text-5xl font-bold mb-24 experience-header">
+    I have experience in
+</div>
+
+<div class="flex flex-col gap-4">
+    <div class="flex flex-row items-center gap-12">
+        <div class="text-5xl">Svelte</div>
+        <Icon icon="logos:svelte-icon" width="100" />
+        <div class="mockup-browser border bg-base-300">
+            <div class="mockup-browser-toolbar">
+                <div class="input">https://daisyui.com</div>
+            </div>
+            <div class="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+        </div>
+    </div>
+
+    <div class="flex flex-row items-center gap-12">
+        <div class="text-5xl">React</div>
+        <Icon icon="logos:react" width="100" />
+        <div class="mockup-browser border bg-base-300">
+            <div class="mockup-browser-toolbar">
+                <div class="input">https://daisyui.com</div>
+            </div>
+            <div class="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
         </div>
     </div>
 </div>
