b5e446bb60023f0a34c52ab020d8da4dc1ba3a0f
Magnus Strømseng <magnus.stromseng@gmail.com>
2024-01-18 12:31:27 +0100

    setup vercel adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,11 +11,11 @@ const config = {
         adapter: adapter({
             // default options are shown. On some platforms
             // these options are set automatically — see below
-            pages: "build",
-            assets: "build",
-            fallback: "index.html",
-            precompress: false,
-            strict: true,
+            // pages: "build",
+            // assets: "build",
+            // fallback: "index.html",
+            // precompress: false,
+            // strict: true,
         }),
     },
 };
