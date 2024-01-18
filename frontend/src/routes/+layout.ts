3130957715f7dbea41a8e6d5587d125a6990f312
Magnus Strømseng <magnus.stromseng@gmail.com>
2024-01-18 13:50:35 +0100

    setup vercel speed insights

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -2,3 +2,7 @@ import { dev } from "$app/environment";
 import { inject } from "@vercel/analytics";
 
 inject({ mode: dev ? "development" : "production" });
+
+import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+
+injectSpeedInsights();
