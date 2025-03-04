1b46f4c256d796009c53aa8db671be192f300fae
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-03-04 14:55:49 +0100

    update age to use current year

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -54,12 +54,16 @@
     <div class="w-full gap-2 prose mb-16">
         <h2 class="">About Me</h2>
         <p class="text-l">
-            I'm a 22 year old Informatics student from Oslo, Norway. I'm
-            currently in the first year of my master's degree at the Norwegian
-            University of Technology and Science. I'm passionate about
-            technology and I love to learn new things. I'm also a big fan of
-            open source software and I try to contribute to open source projects
-            whenever I can. You can read more about me <a href="/about">here</a>
+            I'm a {new Date().getFullYear() -
+                2002 -
+                (new Date() < new Date(new Date().getFullYear(), 3, 19)
+                    ? 1
+                    : 0)} year old Informatics student from Oslo, Norway. I'm currently
+            in the first year of my master's degree at the Norwegian University of
+            Technology and Science. I'm passionate about technology and I love to
+            learn new things. I'm also a big fan of open source software and I try
+            to contribute to open source projects whenever I can. You can read more
+            about me <a href="/about">here</a>
         </p>
     </div>
 
